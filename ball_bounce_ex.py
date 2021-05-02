f86285482074037ac3b9c7b165aaab45a7213f78
NickMateus11 <mateus.nicholas@gmail.com>
2021-05-02 14:50:32 -0400

    update

--- ball_bounce_ex.py
+++ ball_bounce_ex.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+from random import randint
 
 
 w, h = 800, 600
@@ -9,40 +10,55 @@ screen = pygame.display.set_mode((w, h))
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
 
+class Coord():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Ball():
+    def __init__(self, start_pos):
+        self.pos = Coord(*start_pos)
+        self.vy = 0
+        self.vx = randint(3,10) * (2*randint(0,1)-1)
+        self.trail = []
+    
+    def pos_tuple(self):
+        return (self.pos.x, self.pos.y)
+
 def main():
     g = -1
-    v = 0
-    hv = 5
     r = 10
-    pos = [0, h]
-    points = []
+    dt = 0.05
+    
+    num_balls = 30
+    balls = [Ball( (randint(r,w-r), randint(r,h-r)) ) for _ in range(num_balls)]
 
     running = True
     while running:
-        time.sleep(0.1)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         
         screen.fill(BLACK)
 
-        if pos[1]-v > h-r:
-            v = 20
+        for b in balls:
+            if b.pos.y - (b.vy * dt) > h-r:
+                b.vy *= -1
 
-        pos[1] -= v
-        v += g
+            b.pos.y -= b.vy * dt
+            b.vy += g*dt
 
-        pos[0] += hv
+            b.pos.x += b.vx * dt
+            if b.pos.x > w-r or b.pos.x < r:
+                b.vx *= -1
 
-        pygame.draw.circle(screen, WHITE, pos, r, 1)
-        points.append(tuple(pos))
-        if pos[0]+hv > w-r or pos[0]+hv < r:
-            hv *= -1
+            pygame.draw.circle(screen, WHITE, b.pos_tuple(), r, 1)
+            b.trail.append(b.pos_tuple()) 
 
-        if len(points) > 2:
-            pygame.draw.lines(screen, WHITE, False, points, 1)
-        if len(points) > 100:
-            points.pop(0)
+            if len(b.trail) > 2:
+                pygame.draw.lines(screen, WHITE, False, b.trail, 1)
+            if len(b.trail) > 50:
+                b.trail.pop(0)
 
         pygame.display.flip()
     
--- velocity_control_ex.py
+++ velocity_control_ex.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import math
 
 
 w, h = 800, 600
@@ -9,13 +10,38 @@ screen = pygame.display.set_mode((w, h))
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
 
+def dist(A, B):
+    return ((A[0]-B[0])**2 + (A[1]-B[1])**2)**0.5
+def mag(A):
+    return dist( (0,0), A)
+
+def draw_arrow(start, end):
+    dir_v = (end[0]-start[0], end[1]-start[1])
+    if mag(dir_v) < 1:
+        return 
+    unit_v = (dir_v[0] / mag(dir_v), dir_v[1] / mag(dir_v))
+
+    scale = min(0.2,  (min(w,h) / 10) / mag(dir_v))
+    mid_p = ( (end[0] - dir_v[0]*scale), (end[1] - dir_v[1]*scale) )
+    arm_mag_perp = dist(end,mid_p) * math.tan(math.pi/6)
+
+    lines = [
+        (start, end),
+        (end, (-arm_mag_perp*unit_v[1] + mid_p[0],  arm_mag_perp*unit_v[0] + mid_p[1])),
+        (end, ( arm_mag_perp*unit_v[1] + mid_p[0], -arm_mag_perp*unit_v[0] + mid_p[1])),
+    ]
+    for pair in lines:
+        pygame.draw.line(screen, WHITE, *pair)
+
 def main():
     vx = 0
     vy = 0
     vmax = 10
     r = 10
+    dt = 0.05
+    d_speed = 0.05
     pos = [w/2, h/2]
-    points = []
+    trail = []
 
     running = True
     while running:
@@ -24,37 +50,41 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
-        time.sleep(0.1)
-        
+                
         keyPressed = pygame.key.get_pressed()
         if keyPressed[pygame.K_LEFT]:
-            vx += -2
+            vx += -d_speed
         if keyPressed[pygame.K_RIGHT]:
-            vx += 2
+            vx += d_speed
         if keyPressed[pygame.K_UP]:
-            vy += -2
+            vy += -d_speed
         if keyPressed[pygame.K_DOWN]:
-            vy += 2      
+            vy += d_speed     
 
         if abs(vx) > vmax:
             vx = vmax if vx>0 else -vmax
         if abs(vy) > vmax:
             vy = vmax if vy>0 else -vmax
 
-        pos[1] += vy
-        vy *= 0.7
+        pos[1] += vy*dt
+        vy -= d_speed*vy*dt 
 
-        pos[0] += vx
-        vx *= 0.7
+        pos[0] += vx*dt
+        vx -= d_speed*vx*dt 
+
+        v_scale = 5*r/vmax
+        v_vector = ( pos[0] + v_scale*vx, pos[1] + v_scale*vy )
+        draw_arrow(pos, v_vector)
 
         pygame.draw.circle(screen, WHITE, pos, r, 1)
-        points.append(tuple(pos))
 
-        if len(points) > 2:
-            pygame.draw.lines(screen, WHITE, False, points, 1)
-        if len(points) > 20:
-            points.pop(0)
+        # if (abs(vy)>0.5 or abs(vx)>0.5):
+        #     trail.append(tuple(pos))
+
+        # if len(trail) > 2:
+        #     pygame.draw.lines(screen, WHITE, False, trail, 1)
+        # if len(trail) > 50 or (trail and abs(vy)<0.5 and abs(vx)<0.5):
+        #     trail.pop(0)
         
         pygame.display.flip()
     
